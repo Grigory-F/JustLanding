f2e3c3ef6cad39eaa57f4b13ba5af23aeb139991
grishka2500@gmail.com <57104420+Grigory-F@users.noreply.github.com>
2022-05-03 23:16:21 +0300

    new init

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+
+
+
 const input = document.querySelector(".input-file");
 const outputContainer = document.querySelector(".output-container");
 input.addEventListener("change", (event) => {
@@ -33,65 +37,3 @@ input.addEventListener("change", (event) => {
     })
 
 })
-
-/* insertAdjacentHTML("afterbegin",
-                `<div class="output-container__box">
-                    <img src="${eventFile.target.result}" class="output-container__image output-container__image--hover"/>
-                </div >
-                 `
-                ) */
-/* <script>
-import axios from "axios";
-export default {
-  data() {
-    return {
-      arrayFiles: [],
-      files: null,
-      unraw: null,
-    };
-  },
-  methods: {
-    async fething() {
-      const fethingg = await fetch("/api/posts");
-      const data = await fethingg.json();
-      console.table(data);
-    },
-    changeImages(e) {
-      this.unraw = e.target.files;
-      this.files = Array.from(e.target.files);
-      this.files.forEach((file) => {
-        console.log(this.unraw.name);
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = (event) => {
-          this.arrayFiles.push(event.target.result);
-          this.arrayFiles.filter((item, index) => {
-            console.log(item);
-            return this.arrayFiles.indexOf(item) == index;
-          });
-        };
-      });
-    },
-    async onsubmit() {
-      let formData = new FormData();
-      this.unraw.forEach((thatFile, index) => {
-        let file = this.unraw[index];
-        formData.append(thatFile.name, file);
-      });
-
-      axios
-        .post("/api/upload", formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        })
-        .then(() => {
-          console.log("SUCCESS!!");
-        });
-    },
-    deleteImage(images) {
-      this.arrayFiles.splice(this.arrayFiles.indexOf(images), 1);
-    },
-  },
-};
-</script> */
